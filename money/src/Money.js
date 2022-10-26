c72b8d961ba3d87fa42369dcbb66ce33d50a610a
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-10-26 17:54:26 -0300

    React_11_Money Parte 5: Money.js

---
+++ money/src/Money.js
@@ -0,0 +1,38 @@
+import React from "react";
+import useGet from "./useGet";
+import usePost from "./usePost";
+import useRemove from "./useRemove";
+
+const url = "https://money-7d9fd-default-rtdb.firebaseio.com/delete.json";
+
+function Money() {
+  const data = useGet(url);
+  const data2 = useGet("https://httpbin.org/ip");
+  const [postData, post] = usePost(url);
+  const [removeData, remove] = useRemove(url);
+
+  const saveNew = () => {
+    post({ chave: "value", key: "value1", valor: 10 });
+  };
+
+  const deleteNew = () => {
+    remove("https://money-7d9fd-default-rtdb.firebaseio.com/delete/.json");
+  };
+
+  if (data.loading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div className="Money">
+      <h1>Money</h1>
+      {JSON.stringify(data)}
+      <pre>{JSON.stringify(data2)}</pre>
+      <button onClick={saveNew}>Save</button>
+      <pre>{JSON.stringify(postData)}</pre>
+      <button onClick={deleteNew}>Delete</button>
+      <pre>{JSON.stringify(removeData)}</pre>
+    </div>
+  );
+}
+export default Money;
--- money/src/index.js
+++ money/src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import Money from "./Money";
 import reportWebVitals from "./reportWebVitals";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 root.render(
   <StrictMode>
-    <App />
+    <Money />
   </StrictMode>
 );
 
