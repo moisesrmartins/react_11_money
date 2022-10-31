b08bb910b4ccb2d702637e728fcf2453a14161c2
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-10-31 11:34:58 -0300

    React_11_Money Parte 9: Table

--- money/src/Money.js
+++ money/src/Money.js
@@ -6,12 +6,6 @@ const url = "https://money-7d9fd-default-rtdb.firebaseio.com/months.json";
 function Money() {
   const data = useGet(url);
 
-  /*
-  if (data.loading) {
-    return <p>Loading...</p>;
-  }
-  */
-
   return (
     <div className="Money">
       <nav className="navbar navbar-light bg-light">
@@ -33,6 +27,70 @@ function Money() {
       </select>
 
       <button>Add Month</button>
+
+      {data.loading && <span>Loading...</span>}
+      {!data.loading && (
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Year</th>
+              <th>Month</th>
+              <th>Input Forecast</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>2019</td>
+              <td>01</td>
+              <td>1000</td>
+            </tr>
+            <tr>
+              <td>2019</td>
+              <td>02</td>
+              <td>1200</td>
+            </tr>
+            <tr>
+              <td>2019</td>
+              <td>03</td>
+              <td>1400</td>
+            </tr>
+            <tr>
+              <td>2019</td>
+              <td>04</td>
+              <td>1600</td>
+            </tr>
+          </tbody>
+          <thead>
+            <tr>
+              <th>Year</th>
+              <th>Month</th>
+              <th>Input Forecast</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>2020</td>
+              <td>01</td>
+              <td>2000</td>
+            </tr>
+            <tr>
+              <td>2020</td>
+              <td>02</td>
+              <td>2200</td>
+            </tr>
+            <tr>
+              <td>2020</td>
+              <td>03</td>
+              <td>2400</td>
+            </tr>
+            <tr>
+              <td>2020</td>
+              <td>04</td>
+              <td>2600</td>
+            </tr>
+          </tbody>
+        </table>
+      )}
 
       {JSON.stringify(data)}
     </div>
