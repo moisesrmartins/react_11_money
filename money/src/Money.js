b35bbdac3ee849bbe763a3d130890dd799bbaf2b
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-11-01 09:26:11 -0300

    React_11_Money Parte 12: Alteração de Months para Year

--- money/src/Money.js
+++ money/src/Money.js
@@ -1,27 +1,23 @@
 import React from "react";
 import Header from "./elements/Header";
-import Months from "./Months";
+import Year from "./Year";
 
 function Money() {
   return (
     <div className="Money">
       <Header />
       <div className="container">
-        <h2>Add Month</h2>
+        <h2>Add Year</h2>
         <select>
           <option value="2019">2019</option>
           <option value="2020">2020</option>
-        </select>
-        <select>
-          <option value="01">01</option>
-          <option value="02">02</option>
-          <option value="03">03</option>
-          <option value="04">04</option>
+          <option value="2021">2021</option>
+          <option value="2022">2022</option>
         </select>
 
-        <button>Add Month</button>
+        <button>Add Year</button>
 
-        <Months />
+        <Year />
       </div>
     </div>
   );
---
+++ money/src/Year.js
@@ -0,0 +1,43 @@
+import React from "react";
+import useGet from "./useGet";
+
+const url = "https://money-7d9fd-default-rtdb.firebaseio.com/year.json";
+
+const Year = () => {
+  const data = useGet(url);
+
+  if (data.loading) {
+    return <span>Loading...</span>;
+  }
+
+  if (data.data) {
+    return (
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Year</th>
+            <th>Input</th>
+            <th>Input Forecast</th>
+            <th>Output</th>
+            <th>Output Forecast</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.keys(data.data).map((year) => {
+            return (
+              <tr key={year}>
+                <td>{year}</td>
+                <td>{data.data[year].input}</td>
+                <td>{data.data[year].input_forecast}</td>
+                <td>{data.data[year].output}</td>
+                <td>{data.data[year].output_forecast}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    );
+  }
+};
+
+export default Year;
