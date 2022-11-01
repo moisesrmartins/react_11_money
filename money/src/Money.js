fce19013d83c28c0f5828cadfbe4623b304122b9
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-11-01 11:02:42 -0300

    React_11_Money Parte 14: React-router-dom

--- money/src/Money.js
+++ money/src/Money.js
@@ -1,18 +1,27 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Header from "./elements/Header";
 import Year from "./Year";
 import AddYear from "./AddYear";
 
+const Home = () => {
+  return (
+    <div className="container">
+      <AddYear />
+      <Year />
+    </div>
+  );
+};
+
 function Money() {
   return (
-    <div className="Money">
-      <Header />
-      <div className="container">
-        <AddYear />
-
-        <Year />
+    <Routes>
+      <div className="Money">
+        <Header />
+        <Route path="/" element={<Home />} />
+        <Home />
       </div>
-    </div>
+    </Routes>
   );
 }
 export default Money;
