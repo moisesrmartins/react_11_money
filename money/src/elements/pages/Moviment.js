24f69942798137070dca0aa2b1fa272977c932ae
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-11-04 15:16:22 -0300

    React_11_Money Parte 21: rest, useParams e stringfy

--- money/src/elements/pages/Moviment.js
+++ money/src/elements/pages/Moviment.js
@@ -1,10 +1,19 @@
 import React from "react";
+import rest from "../use/rest";
+import { useParams } from "react-router-dom";
 
-const Moviment = () => {
+const baseURL = "https://money-7d9fd-default-rtdb.firebaseio.com/";
+const { useGet } = rest(baseURL);
+
+const Moviment = ({ match }) => {
+  let { id } = useParams(match);
+  const data = useGet("moviment", id);
   return (
-    <div>
+    <div className="container">
       <h1>Moviment</h1>
       <h2>Money</h2>
+      <pre>{JSON.stringify(data)}</pre>
+      <pre>{JSON.stringify(id)}</pre>
     </div>
   );
 };
