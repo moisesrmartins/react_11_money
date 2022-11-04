a21c51d53762cf72e3d1780923c17bdf0a8eb8ec
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-11-04 15:52:02 -0300

    React_11_Money Parte 22: Loadiang e table

--- money/src/elements/pages/Moviment.js
+++ money/src/elements/pages/Moviment.js
@@ -8,14 +8,46 @@ const { useGet } = rest(baseURL);
 const Moviment = ({ match }) => {
   let { id } = useParams(match);
   const data = useGet("moviment", id);
-  return (
-    <div className="container">
-      <h1>Moviment</h1>
-      <h2>Money</h2>
-      <pre>{JSON.stringify(data)}</pre>
-      <pre>{JSON.stringify(id)}</pre>
-    </div>
-  );
+
+  if (data.loading) {
+    return <span>Loading...</span>;
+  }
+
+  if (data.data) {
+    return (
+      <div className="container">
+        <h1>Moviment</h1>
+        <h2>Money</h2>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Moviment</th>
+              <th>Wage One</th>
+              <th>Wage Two</th>
+              <th>Wage Three</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Object.keys(data.data).map((moviment) => {
+              return (
+                <tr key={moviment}>
+                  <td to={`/moviment/${moviment}`}>{moviment}</td>
+                  <td>{data.data[moviment].wage_one}</td>
+                  <td>{data.data[moviment].wage_two}</td>
+                  <td>{data.data[moviment].wage_three}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+
+        <pre>{JSON.stringify(data)}</pre>
+        <pre>{JSON.stringify(data.id)}</pre>
+        <pre>{JSON.stringify(id)}</pre>
+      </div>
+    );
+  }
+  return null;
 };
 
 export default Moviment;
