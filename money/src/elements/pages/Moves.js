9b0670109eaed85d37598e039bf4344781ab3b07
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-11-08 15:52:39 -0300

    React_11_Money Parte 28: rest e Loading

--- money/src/elements/pages/Moves.js
+++ money/src/elements/pages/Moves.js
@@ -1,5 +1,15 @@
 import React from "react";
+import rest from "../use/rest";
 
-const Moves = () => {};
+const baseURL = "https://money-7d9fd-default-rtdb.firebaseio.com/";
+const { useGet } = rest(baseURL);
+
+const Moves = () => {
+  const data = useGet("moves");
+
+  if (data.loading) {
+    return <span>Loading...</span>;
+  }
+};
 
 export default Moves;
