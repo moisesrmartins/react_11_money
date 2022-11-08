d147cb25cc01746dc61c0a53446c3f4f525b44ec
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-11-08 15:54:28 -0300

    React_11_Money Parte 30: Link

--- money/src/elements/pages/Moves.js
+++ money/src/elements/pages/Moves.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import rest from "../use/rest";
 
 const baseURL = "https://money-7d9fd-default-rtdb.firebaseio.com/";
@@ -14,7 +15,11 @@ const Moves = () => {
   return (
     <div className="container">
       <h1>Moves</h1>
-      <h2>Moviment</h2>
+      <h2>
+        <Link to={"/moviment/:data"} style={{ textDecoration: "none" }}>
+          Moviment
+        </Link>
+      </h2>
     </div>
   );
 };
