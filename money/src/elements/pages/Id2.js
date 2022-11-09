c50e00426f1426c1665cc8fb38a059e347aedd78
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-11-09 19:08:57 -0300

    React_11_Money Parte 33.4: data, container e Link

--- money/src/elements/pages/Id2.js
+++ money/src/elements/pages/Id2.js
@@ -1,5 +1,6 @@
 import React from "react";
 import rest from "../use/rest";
+import { Link } from "react-router-dom";
 
 const baseURL = "https://money-7d9fd-default-rtdb.firebaseio.com/";
 const { useGet } = rest(baseURL);
@@ -10,6 +11,21 @@ const Id2 = () => {
   if (data.loading) {
     return <span>Loading...</span>;
   }
+
+  if (data.data) {
+    return (
+      <div className="container">
+        <h1>Bank Moviment</h1>
+        <h2>
+          <Link to={"/moves"} style={{ textDecoration: "none" }}>
+            Moves
+          </Link>
+        </h2>
+      </div>
+    );
+  }
+
+  return null;
 };
 
 export default Id2;
