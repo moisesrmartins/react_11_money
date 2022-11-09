b9cfd855f1846acf2189fc600bd00bbe4bcfd99f
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-11-09 18:32:02 -0300

    React_11_Money Parte 33.3: Loading, useGet e Route

--- money/src/elements/pages/Id2.js
+++ money/src/elements/pages/Id2.js
@@ -1,5 +1,15 @@
 import React from "react";
+import rest from "../use/rest";
 
-const Id1 = () => {};
+const baseURL = "https://money-7d9fd-default-rtdb.firebaseio.com/";
+const { useGet } = rest(baseURL);
 
-export default Id1;
+const Id2 = () => {
+  const data = useGet("id2");
+
+  if (data.loading) {
+    return <span>Loading...</span>;
+  }
+};
+
+export default Id2;
