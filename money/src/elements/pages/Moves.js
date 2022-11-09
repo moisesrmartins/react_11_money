4c347b655a5ad084ab4b2888b0e861db2d07f8c3
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-11-09 14:46:54 -0300

    React_11_Money Parte 31: if, thead, tbody e return null

--- money/src/elements/pages/Moves.js
+++ money/src/elements/pages/Moves.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import rest from "../use/rest";
+import { Link } from "react-router-dom";
 
 const baseURL = "https://money-7d9fd-default-rtdb.firebaseio.com/";
 const { useGet } = rest(baseURL);
@@ -12,16 +12,45 @@ const Moves = () => {
     return <span>Loading...</span>;
   }
 
-  return (
-    <div className="container">
-      <h1>Moves</h1>
-      <h2>
-        <Link to={"/moviment/:data"} style={{ textDecoration: "none" }}>
-          Moviment
-        </Link>
-      </h2>
-    </div>
-  );
+  if (data.data) {
+    return (
+      <div className="container">
+        <h1>Moves</h1>
+        <h2>
+          <Link to={"/moviment/:data"} style={{ textDecoration: "none" }}>
+            Moviment
+          </Link>
+        </h2>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>ID 1</th>
+              <th>ID 2</th>
+            </tr>
+          </thead>
+          <tbody>
+            {Object.keys(data.data).map((moves) => {
+              return (
+                <tr key={moves}>
+                  <td>
+                    <Link style={{ textDecoration: "none" }} to={"/id1"}>
+                      {data.data[moves].id_1}
+                    </Link>
+                  </td>
+                  <td>
+                    <Link style={{ textDecoration: "none" }} to={"/id2"}>
+                      {data.data[moves].id_2}
+                    </Link>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+  return null;
 };
 
 export default Moves;
