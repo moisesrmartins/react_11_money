d723a2693bdb544417c347e40fdc618f179bd576
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-11-17 10:39:12 -0300

    React_11_Money Parte 41: useRef

--- money/src/elements/pages/home/AddYear.js
+++ money/src/elements/pages/home/AddYear.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useRef } from "react";
 
 const minYear = 2019;
 const maxYear = 2022;
 
 const AddYear = () => {
+  const refYear = useRef();
+
   const years = [];
   for (let i = minYear; i <= maxYear; i++) {
     years.push(i);
@@ -12,12 +14,15 @@ const AddYear = () => {
   return (
     <div>
       <h2>Add Year</h2>
-      <select>
-        <option value="2019">2019</option>
-        <option value="2020">2020</option>
-        <option value="2021">2021</option>
-        <option value="2022">2022</option>
+
+      <select ref={refYear}>
+        {years.map((year) => (
+          <option key={year} value={year}>
+            {year}
+          </option>
+        ))}
       </select>
+
       <button>Add Year</button>
     </div>
   );
