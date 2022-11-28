243e462e21b5a9d99055b9b4ec110cd4e5f022d0
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-11-28 16:45:42 -0300

    React_11_Money Parte 45: @babel/eslint-parser, index.js e .eslintrc.js

--- money/functions/index.js
+++ money/functions/index.js
@@ -1,9 +1,31 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+exports.soma = functions.database
+  .ref("/moviment/{day}")
+  .onWrie(async (change, context) => {
+    const yearsRef = admin.database().ref("/year/" + context.params.day);
+    const movimentRef = change.after.ref;
+    const movimentSS = await movimentRef.once("value");
+    const moviment = movimentSS.val();
+
+    let input = 0;
+    let output = 0;
+
+    object.keys(moviment).forEach((m) => {
+      if (moviment[m].value > 0) {
+        input += moviment[m].value;
+      } else {
+        output += moviment[m].value;
+      }
+    });
+
+    return yearsRef.transaction((current) => {
+      if (current === null) {
+        return { input, output, input_forecast: 0, output_forecast: 0 };
+      }
+      return { ...current, input, output };
+    });
+  });
